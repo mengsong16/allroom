0ef74e660aff4e424dc16af4c9b6eaba6d20a797
mengsong16 <mes050@ucsd.edu>
2023-01-13 14:02:02 -0800

    organize experiments

--- allroom/experiments/run_experiments.py
+++ allroom/experiments/run_experiments.py
@@ -1,18 +1,52 @@
 from all.experiments import run_experiment, plot_returns_100
-from all.presets.classic_control import dqn
+#from all.presets.classic_control import dqn
 from all.environments import GymEnvironment
-
+from all.presets.builder import PresetBuilder
+from all.presets.classic_control.models import fc_relu_q
+from all.presets.classic_control.dqn import DQNClassicControlPreset
+from allroom.utils.path import *
 
 def main():
-    device = 'cuda'
-    timesteps = 40000
+    hyperparameters = {
+        # Common settings
+        "discount_factor": 0.99,
+        # Adam optimizer settings
+        "lr": 1e-3,
+        # Training settings
+        "minibatch_size": 64,
+        "update_frequency": 1,
+        "target_update_frequency": 100,
+        "timesteps": 40000,
+        # Replay buffer settings
+        "replay_start_size": 1000,
+        "replay_buffer_size": 10000,
+        # Explicit exploration
+        "initial_exploration": 1.,
+        "final_exploration": 0.,
+        "final_exploration_step": 10000,
+        "test_exploration": 0.001,
+        # Model construction
+        "model_constructor": fc_relu_q,
+    }
+    
+    # the hyperparameter dictionary should not include keywords device and name
+    device = "cuda"
+    # need to explicitly ensure the agent and env are on the same device
+    agent = PresetBuilder(default_name='dqn', 
+            default_hyperparameters=hyperparameters, 
+            constructor=DQNClassicControlPreset,
+            device=device)
 
     run_experiment(
-        agents=[dqn.device(device)],
+        agents=[agent],
         envs=[GymEnvironment(id='CartPole-v0', device=device)],
-        frames=timesteps,
+        frames=hyperparameters["timesteps"],
+        logdir=runs_path,
+        quiet=False, # if False print info to standard output 
+        verbose=True, # whether or not to log all data or only summary metrics
     )
-    plot_returns_100('runs', timesteps=timesteps)
+
+    plot_returns_100(runs_path, timesteps=hyperparameters["timesteps"])
 
 if __name__ == "__main__":
     main()
--- allroom/utils/path.py
+++ allroom/utils/path.py
@@ -5,8 +5,7 @@ import numpy as np
 
 cur_path = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.join(cur_path[:cur_path.find("/allroom")], "allroom")
-data_path = os.path.join(root_path, "data") 
-checkpoints_path = os.path.join(root_path, "checkpoints")
+runs_path = os.path.join(root_path, "runs")
 config_path = os.path.join(root_path, "configs")
 figure_path = os.path.join(root_path, "figures")
 tensorboard_path = os.path.join(root_path, "tensorboard")
